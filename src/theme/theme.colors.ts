edf9e50424583d682a251504098c37996a684878
nils0n <nilson_jose.v@hotmail.com>
2023-04-03 22:25:45 -0300

    feat: create theme colors

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary:'#212529',
+  text:{
+    white:'#F8F9FA',
+    dark:'#343A40'
+  },
+  backgroud:{
+    dark:'#212529',
+    white:'#F8F9FA',
+  },
+  input:{
+    background:'#E9ECEF',
+    placeholder:'#6C757D'
+  },
+  error:'#FF6A5A'
+};
+
+export default Colors;
