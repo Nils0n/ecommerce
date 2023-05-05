19b5a41fd3835bf0fe63a24d2fcee8c0ad96dfee
nils0n <nilson_jose.v@hotmail.com>
2023-05-05 15:31:09 -0300

    feat: create UserContext

---
+++ src/contexts/UserContext/index.tsx
@@ -0,0 +1,47 @@
+import { createContext, ReactNode, useState } from 'react'
+import IUserType from '../../types/user.types';
+
+
+interface IUserContext {
+  currentUser: IUserType | null;
+  isAuthenticated: boolean;
+  loginUser: (user: IUserType) => void;
+  logoutUser: () => void;
+}
+
+interface IUserContextProviderProps {
+  children: ReactNode;
+}
+
+
+export const UserContext = createContext<IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => { },
+  logoutUser: () => { }
+
+});
+
+
+function UserContextProvider({ children }: IUserContextProviderProps) {
+
+  const [currentUser, setCurrentUser] = useState<IUserType | null>(null);
+  const isAuthenticated = currentUser !== null;
+
+
+  function loginUser(user: IUserType) {
+    setCurrentUser(user);
+  }
+
+  function logoutUser() {
+    setCurrentUser(null);
+  }
+
+  return (
+    <UserContext.Provider value={{ currentUser, isAuthenticated, loginUser, logoutUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContextProvider;
